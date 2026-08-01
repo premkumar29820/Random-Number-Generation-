bbabc057f5540721e928f859ce8aa3e4aae53a49
premkumar29820 <premkumar2982003@gmail.com>
2026-08-01 14:09:26 +0530

    updated lucky pool

--- src/component/random.js
+++ src/component/random.js
@@ -3,13 +3,28 @@ import { useState } from "react";
 
 function RandomNumber() {
   var [random, setRandom] = useState(null)
+   const [luckypool, setluckypool] = useState("")
 
 
 
   var generate = () => {
         const number = Math.floor(Math.random() * 100) + 1;
         setRandom(number)
-  }
+  
+
+    if (random === 7) {
+      setluckypool("🍀 Lucky Number!");
+    } else if (
+      random === 2  ||
+      random === 29 ||
+      random === 100
+    ) {
+      setluckypool("🎉 Jackpot!");
+    } else {
+      setluckypool("😢 Try Again!");
+    }
+  };
+
 
     var reset = () => {
     setRandom(null)
@@ -23,7 +38,10 @@ function RandomNumber() {
          {random === null ? (
         <p className="message">No number generated yet</p>
       ) : (
-        <h2 className="number">{random}</h2>
+      <>
+       <h2 className="number">{random}</h2>
+       <h2>{luckypool}</h2>
+      </>
       )}
 
       <div className="buttons">
